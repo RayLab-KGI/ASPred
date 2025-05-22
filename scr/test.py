08de5a8587a0f42c07292e6db140717870e1b840
Sebastian Bassi <sebastian@toyoko.io>
2025-05-21 20:13:50 -0700

    Update test.py

--- scr/test.py
+++ scr/test.py
@@ -7,16 +7,24 @@ from data_processing.preprocess import preprocess_sequences
 from utils import create_dataset, evaluate
 from torch.utils.data import DataLoader
 
-def predict(model, tokenizer, test_df, batch_size=16):
-    test_set = create_dataset(tokenizer, list(test_df['sequence']), list(test_df['label']))
+def predict(model_path, test_df, batch_size=16):
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    tokenizer, model = load_model(model_path, num_labels=2, mixed=False)
+    model.to(device)
+    data = pd.read_csv(test_df)
+
+    data_df = preprocess_sequences(data, sequence_col="sequence")
+    test_set = create_dataset(tokenizer, list(data_df['sequence']), list(data_df['label']))
     test_dataloader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
     model.eval()
 
     predicted_probs, predicted_labels = [], []
     with torch.no_grad():
         for batch in test_dataloader:
-            input_ids = batch['input_ids']
-            attention_mask = batch['attention_mask']
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
             logits = model(input_ids, attention_mask=attention_mask).logits
             probs = torch.softmax(logits, dim=-1)[:, 1]
             labels = (probs >= 0.5).long()
@@ -24,9 +32,12 @@ def predict(model, tokenizer, test_df, batch_size=16):
             predicted_labels.extend(labels.tolist())
     return predicted_probs, predicted_labels
 
-def evaluate_model(model_path, tokenizer_path, data_path, num_labels=1):
+def evaluate_model(model_path, data_path, num_labels=1):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # Load the fine-tuned model and tokenizer
+
     tokenizer, model = load_model(model_path, num_labels=num_labels, mixed=False)
+    model.to(device)
     model.eval()
 
     # Load and preprocess the dataset
